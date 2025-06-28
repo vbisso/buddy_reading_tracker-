841e56bf453945829269dd9b5f0f2caab7d9dfc6
Braden Hamilton <zmrtzer@byui.edu>
2025-06-27 18:54:52 -0600

    cors update

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const { connect } = require("./config/db");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger/swagger-output.json");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
