7f3d769ddfb3ee5ed80c1570aa7e3151ccf255a5
Braden Hamilton <zmrtzer@byui.edu>
2025-06-27 18:53:21 -0600

    Render Update

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,12 +1,16 @@
 const swaggerAutogen = require("swagger-autogen")();
 
+//Dynamically choose host
+const isRender = process.env.RENDER === "true";
+const host = isRender ? "cse341-tpkb.onrender.com" : "localhost:3000";
+
 const doc = {
   info: {
     title: "Buddy Reading Tracker API",
     description: "API for managing books, users, progress, and notes",
   },
-  host: "localhost:3000", // Change this to your Render domain when deploying
-  schemes: ["http"], // Use https if deploying to Render
+  host: host,
+  schemes: [isRender ? "https" : "http"],
 };
 
 const outputFile = "./swagger/swagger-output.json";
