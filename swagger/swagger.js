8ae1d4377d88e1a74893edf475284fa65d04dadf
Braden Hamilton <zmrtzer@byui.edu>
2025-07-05 13:03:54 -0600

    OAuth update

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -2,7 +2,10 @@ const swaggerAutogen = require("swagger-autogen")();
 
 //Dynamically choose host
 const isRender = process.env.RENDER === "true";
-const host = isRender ? "buddy-reading-tracker.onrender.com" : "localhost:3000";
+process.env.RENDER_EXTERNAL_HOSTNAME || false;
+const host = isRender ? "cse341-tpkb.onrender.com" : "localhost:3000";
+
+console.log("Using host:", host);
 
 const doc = {
   info: {
@@ -11,8 +14,24 @@ const doc = {
   },
   host: host,
   schemes: [isRender ? "https" : "http"],
+  securityDefinitions: {
+    bearerAuth: {
+      type: "apiKey",
+      name: "Authorization",
+      in: "header",
+      description: "Enter JWT as: Bearer <token>",
+    },
+  },
+  security: [{ bearerAuth: [] }],
 };
 
 const outputFile = "./swagger/swagger-output.json";
-const endpointsFiles = ["./routes/index.js"];
+const endpointsFiles = [
+  "./server.js",
+  "./routes/books.js",
+  "./routes/users.js",
+  "./routes/readingProgress.js",
+  "./routes/highlightsNotes.js",
+];
+
 swaggerAutogen(outputFile, endpointsFiles, doc);
