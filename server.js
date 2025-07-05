ae1f0d55215c5a350cffd720827c5773e496a645
Braden Hamilton <zmrtzer@byui.edu>
2025-07-05 13:04:22 -0600

    OAuth update

--- server.js
+++ server.js
@@ -2,15 +2,40 @@ const express = require("express");
 const { connect } = require("./config/db");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger/swagger-output.json");
+const cors = require("cors");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+
+require("./config/passport");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_KEY || "key"],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Fake Auth for testing
+if (process.env.USE_FAKE_AUTH === "true") {
+  const fakeAuthRoutes = require("./routes/fakeAuth");
+  app.use("/fakeauth", fakeAuthRoutes);
+  console.log("Using fake auth for testing.");
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/", require("./routes"));
 
 connect().then(() => {
