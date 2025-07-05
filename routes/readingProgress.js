c3c1d17259bf111f9ee3347b96a2a383c8fa116b
Braden Hamilton <zmrtzer@byui.edu>
2025-07-05 13:02:44 -0600

    Authentication

--- routes/readingProgress.js
+++ routes/readingProgress.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const readingProgressController = require("../controllers/readingProgress");
+const authenticate = require("../middleware/authenticate");
 
-router.get("/", readingProgressController.getAllReadingProgress);
-router.get("/:id", readingProgressController.getReadingProgressById);
-router.post("/", readingProgressController.addReadingProgress);
-router.put("/:id", readingProgressController.updateReadingProgress);
-router.delete("/:id", readingProgressController.deleteReadingProgress);
+router.get("/", authenticate, readingProgressController.getAllReadingProgress);
+router.get(
+  "/:id",
+  authenticate,
+  readingProgressController.getReadingProgressById
+);
+router.post("/", authenticate, readingProgressController.addReadingProgress);
+router.put(
+  "/:id",
+  authenticate,
+  readingProgressController.updateReadingProgress
+);
+router.delete(
+  "/:id",
+  authenticate,
+  readingProgressController.deleteReadingProgress
+);
 
 module.exports = router;
