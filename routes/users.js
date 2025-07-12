38d23ab7a7be22d97528266c83c2e0dc3d64a308
Braden Hamilton <zmrtzer@byui.edu>
2025-07-12 10:25:00 -0600

    Validation Update

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/users");
+const { body } = require("express-validator");
+const validate = require("../middleware/validate");
 const authenticate = require("../middleware/authenticate");
+const userController = require("../controllers/users");
+
+const validateUser = [
+  body("username").notEmpty().withMessage("Username is required"),
+  body("email").isEmail().withMessage("Valid email is required"),
+  validate,
+];
 
 router.get("/", authenticate, userController.getAllUsers);
 router.get("/:id", authenticate, userController.getUserById);
-router.post("/", authenticate, userController.createUser);
+router.post("/", authenticate, validateUser, userController.createUser);
+router.put("/:id", authenticate, validateUser, userController.updateUser);
 router.delete("/:id", authenticate, userController.deleteUser);
-router.put("/:id", authenticate, userController.updateUser);
 
 module.exports = router;
